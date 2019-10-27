383a5b0f0403723b7660794b658a348eb2aec67a
Nicoleus1 <55134353+nicoleus@users.noreply.github.com>
2019-10-27 16:12:07 +0700

    TBomb.sh

--- TBomb.sh
+++ TBomb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clear
-echo -e "\e[4;31m SpeedX Productions !!! \e[0m"
+echo -e "\e[4;31m Nicoleus Sitorus Productions !!! \e[0m"
 echo -e "\e[1;34m Presents \e[0m"
 echo -e "\e[1;32m TBomb \e[0m"
 echo "Press Enter To Continue"
@@ -14,7 +14,7 @@ echo .
 apt install figlet toilet python curl -y
 apt install python3-pip
 pip install -r requirements.txt
-echo This Script Was Made By SpeedX >update.speedx
+echo This Script Was Made By Nicoleus Sitorus >update.speedx
 echo Requirements Installed....
 echo Press Enter To Continue...
 read upd
@@ -26,10 +26,11 @@ clear
 echo -e "\e[1;31m"
 figlet TBomb
 echo -e "\e[1;34m Created By \e[1;32m"
-toilet -f mono12 -F border SpeedX
-echo -e "\e[4;34m This Bomber Was Created By SpeedX \e[0m"
+toilet -f mono12 -F border Nicoleus Sitorus
+echo -e "\e[4;34m This Bomber Was Created By Nicoleus Sitorus \e[0m"
 echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
 echo -e "\e[1;32m           Mail: adysitorusa7@gmail.com \e[0m"
+echo -e "\e[1;32m FACEBOOK : Nicoleus Sitorus \e[0m"
 echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/ady sitorus ady \e[0m"
 echo " "
 echo -e "\e[4;31m Please Read Instruction Carefully !!! \e[0m"
@@ -50,7 +51,7 @@ exit 0
 elif [ $ch -eq 2 ];then
 clear
 echo -e "\e[1;32m"
-echo 'Call Bomb By SpeedX'> call.xxx
+echo 'Call Bomb By Nicoleus'> call.xxx
 python3 bomber.py call
 rm *.xxx >/dev/null 2>&1
 exit 0
@@ -58,9 +59,8 @@ elif [ $ch -eq 3 ];then
 clear
 apt install git -y
 echo -e "\e[1;34m Downloading Latest Files..."
-git clone https://github.com/TheSpeedX/TBomb
-if [[ -s TBomb/TBomb.sh ]];then
-cd TBomb
+git clone https://github.com/nicoleus/TBombSpammer-Grab
+cd Spammer-Grab
 cp -r -f * .. > temp
 cd ..
 rm -rf  TBomb >> temp
@@ -79,7 +79,7 @@ clear
 echo -e "\e[1;33m"
 figlet TBomb
 echo -e "\e[1;34mCreated By \e[1;34m"
-toilet -f mono12 -F border SpeedX
+toilet -f mono12 -F border Nicoleus Sitorus
 echo  " "
 echo -e "\e[1;32m                   Features\e[1;34m"
 echo "  [+] Unlimited And Super-Fast Bombing"
